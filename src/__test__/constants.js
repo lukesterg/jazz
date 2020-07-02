bc7890b3f3fbe1fa60dff103ef0e100f8e38eb8d
Luke Geraghty <9840170+lukesterg@users.noreply.github.com>
2020-07-02 14:06:54 +0930

    Scrub-a-dub-dub validation

--- src/__test__/constants.js
+++ src/__test__/constants.js
@@ -1,43 +1,45 @@
 import Jazz from '../';
+import scrub from 'scrub-a-dub-dub';
 
 export const defaultModels = {
   class: {
-    id: Jazz.field.number({ primaryKey: true }),
-    name: {},
-    teacher: {},
-    funding: {},
-    helper: {},
+    id: Jazz.field.primaryKey(),
+    name: scrub.string(),
+    teacher: scrub.string(),
+    funding: scrub.number(),
+    helper: scrub.string(),
     students: Jazz.field.hasMany('student', { relatedField: 'class' }),
   },
   student: {
-    id: Jazz.field.number({ primaryKey: true }),
-    name: {},
-    age: Jazz.field.number(),
+    id: Jazz.field.primaryKey(),
+    name: scrub.string(),
+    age: scrub.number(),
     class: Jazz.field.hasOne('class'),
     address: Jazz.field.hasOne('address'),
   },
   address: {
-    id: Jazz.field.number({ primaryKey: true }),
-    city: {},
+    id: Jazz.field.primaryKey(),
+    city: scrub.string(),
     student: Jazz.field.hasMany('student', { relatedField: 'address' }),
   },
   // playground for inserting data which can be deleted which wont interfere with read tests
   savetest1: {
-    id: Jazz.field.number({ primaryKey: true }),
+    id: Jazz.field.primaryKey(),
+    a: scrub.number(),
   },
   savetest2: {
-    id: Jazz.field.number({ primaryKey: true }),
-    a: {},
-    b: {},
+    id: Jazz.field.primaryKey(),
+    a: scrub.number(),
+    b: scrub.number(),
   },
   savetest3_author: {
-    id: Jazz.field.number({ primaryKey: true }),
-    name: {},
+    id: Jazz.field.primaryKey(),
+    name: scrub.string(),
     books: Jazz.field.hasMany('savetest3_book', { relatedField: 'author' }),
   },
   savetest3_book: {
-    id: Jazz.field.number({ primaryKey: true }),
-    name: {},
+    id: Jazz.field.primaryKey(),
+    name: scrub.string(),
     author: Jazz.field.hasOne('savetest3_author'),
   },
 };
--- src/utilities.js
+++ src/utilities.js
@@ -23,3 +23,38 @@ export const defineNonIterableProperty = (object, key, value) =>
 export const addSymbol = (object, symbol) => defineNonIterableProperty(object, symbol, true);
 
 export const hasSymbol = (object, symbol) => object[symbol] === true;
+
+// Delayed resolution without using promises
+const delayedSymbol = Symbol();
+export const createDelayedObject = () => {
+  const obj = {
+    [delayedSymbol]: true,
+  };
+
+  const accept = (value) => {
+    obj.value = value;
+  };
+
+  return { obj, accept };
+};
+
+export const delayedObjectFlatten = (obj) => {
+  const matches = Object.entries(obj).filter((entry) => entry[1]?.[delayedSymbol]);
+  if (matches.length === 0) {
+    return obj;
+  }
+
+  const copy = { ...obj };
+  matches.forEach(([key, value]) => {
+    copy[key] = value.value;
+  });
+
+  return copy;
+};
+
+export const replaceObject = (destination, source) => {
+  deleteItemsFromObject(destination);
+  Object.assign(destination, source);
+};
+
+const deleteItemsFromObject = (obj) => Object.keys(obj).forEach((key) => delete obj[key]);
